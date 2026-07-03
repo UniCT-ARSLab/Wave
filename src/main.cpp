5980dfed7704a7c2b44763e6a5723e4177eac91d
defenge <damiano.coppola03@gmail.com>
2026-07-03 23:44:18 +0200

    feat(state/main):boat states.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,56 @@
 #include <Arduino.h>
+#include <ios>
+#include <iot_board.h>
+#include <state.h>
 
-// put function declarations here:
-int myFunction(int, int);
+BoatState state;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  IoTBoard::init_display();
+  IoTBoard::init_serial(115200);
+  IoTBoard::init_buttons();
+  Wire.begin();
+  IoTBoard::init_spi();
+
+  display->clearDisplay();
+  display->println("Setup OK");
+  display->display();
+  state = BoatState::Idle;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  switch (state) {
+
+  case BoatState::Idle:
+    display->setCursor(0, 0);
+    display->clearDisplay();
+    display->println("State:Idle");
+    display->display();
+    delay(1000);
+    state = BoatState::Armed;
+    break;
+
+  case BoatState::Armed:
+
+    display->clearDisplay();
+    display->println("State:Armed");
+    display->display();
+    delay(1000);
+    state = BoatState::Alarm;
+
+  case BoatState::Alarm:
+    display->clearDisplay();
+    display->println("State:Alarm");
+    display->display();
+    delay(1000);
+    state = BoatState::Idle;
+    break;
+
+  default:
+    Serial.println("no valid state");
+
+    break;
+  }
 }
